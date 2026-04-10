f402896b4d46b1da1cc170362b571ff461b20cef
juancarlos200 <jk6610687@gmai.com>
2026-04-09 19:06:35 -0600

    Diccionario

---
+++ src/Diccionario.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Diccionario {
+    private Arbol<Asociacion<String, String>> arbol;
+
+    public Diccionario() {
+        arbol = new ArbolBinario<>();
+    }
+
+    public void agregar(String frances, String espanol) {
+        arbol.insertar(new Asociacion<>(frances.toLowerCase(), espanol.toLowerCase()));
+    }
+
+    public String traducir(String frances) {
+        Asociacion<String, String> llave = new Asociacion<>(frances.toLowerCase(), null);
+        Asociacion<String, String> resultado = arbol.buscar(llave);
+        return (resultado != null) ? resultado.getValor() : null;
+    }
+
+    public void imprimirEnOrden() {
+        List<Asociacion<String, String>> lista = new ArrayList<>();
+        arbol.enOrden(lista);
+        for (Asociacion<String, String> a : lista) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+}
